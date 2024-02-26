65275dcb95d6c5c4a5e578dbd770baece6f1fd84
Heisei <92716223+Bagacay@users.noreply.github.com>
2024-02-27 00:44:02 +0800

    2

--- config/connection.php
+++ config/connection.php
@@ -2,16 +2,16 @@
 function connection()
 {
     //LOCAL
-    // $host = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $database = "upkeep";
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "upkeep";
 
     // HOSTED
-    $host = "localhost";
-    $username = "u483250324_UpKeep";
-    $password = ":El7+4xg";
-    $database = "u483250324_UpKeep";
+    // $host = "localhost";
+    // $username = "u483250324_UpKeep";
+    // $password = ":El7+4xg";
+    // $database = "u483250324_UpKeep";
 
     $conn = new mysqli($host, $username, $password, $database);
 
