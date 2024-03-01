0be2fa32c564cac166d81a360bed0380ffa00219
alphard12 <jamesalphardaninion@gmail.com>
2024-03-02 00:24:47 +0800

    Update staff.php

--- users/administrator/staff.php
+++ users/administrator/staff.php
@@ -487,6 +487,23 @@ if (isset($_SESSION['accountId']) && isset($_SESSION['email'])) {
                             </div>
                         </div>
 
+                        <!-- RFID MODAL FOR ADD -->
+                        <div class="modal" id="addRFIDModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-body">
+                                        <img src="../../src/img/taprfid.jpg" width="100%" alt="" class="Scan" />
+
+                                        <form id="rfidForm">
+                                            <input type="text" id="rfid" name="rfid" value="12345">
+                                        </form>
+                                    </div> 
+                                    
+                                    <label class="btn btn-close-modal-emp close-modal-btn" data-bs-toggle="modal" data-bs-target="#exampleModal1"><i class="bi bi-x-lg"></i></label>
+                                </div>
+                            </div>
+                        </div>
+
                         <script>
                             function saveRFIDToValue(event, action) {
                                 event.preventDefault();
@@ -555,7 +572,7 @@ if (isset($_SESSION['accountId']) && isset($_SESSION['email'])) {
 
                                                 <div class="col-4">
                                                     <label for="contactField" class="form-label">Contact Number <span class="d-none text-danger error">*</span></label>
-                                                    <input type="number" class="form-control contactEdit" id="contactField" name="contact" required pattern="\d{10,11}"  value="09" title="Contact number must be 10 to 11 digits long"/>
+                                                    <input type="tel" class="form-control contactEdit" id="contactField" name="contact" required pattern="\d{10,11}"  value="09" title="Contact number must be 10 to 11 digits long"/>
                                                 </div>
 
                                                 <div class="col-4">
@@ -580,7 +597,7 @@ if (isset($_SESSION['accountId']) && isset($_SESSION['email'])) {
 
                                                 <div class="col-4">
                                                     <label for="user_pass" class="form-label">Register RFID <span class="d-none text-danger error">*</span></label>
-                                                    <button type="button" class="form-control btn-custom" data-bs-toggle="modal" data-bs-target="#staffRFIDModal" onclick="setAction('add');" value="123456789">SCAN</button>
+                                                    <button type="button" class="form-control btn-custom" data-bs-toggle="modal" data-bs-target="#addRFIDModal" onclick="setAction('add');" value="123456789">SCAN</button>
                                                     <input type="password" name="rfidNumber" id="rfidFieldAdd" title="" value="1234567" required />
                                                 </div>
 
