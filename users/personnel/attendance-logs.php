3d844233cc4dfd95c9bc183b2f30de080bd37f44
shiki2x <161136705+shikigami2x@users.noreply.github.com>
2024-03-05 10:08:36 +0800

    a
    a

--- users/personnel/attendance-logs.php
+++ users/personnel/attendance-logs.php
@@ -51,7 +51,7 @@ if (isset($_SESSION['accountId']) && isset($_SESSION['email']) && isset($_SESSIO
                   FROM activitylogs AS al
                   JOIN account AS acc ON al.accountID = acc.accountID
                   WHERE al.tab='Report' 
-                  AND al.seen = '0' AND al.action LIKE ?
+                  AND al.p_seen = '0' AND al.action LIKE ?
                   ORDER BY al.date DESC 
                   LIMIT 1000";
 
@@ -66,7 +66,7 @@ if (isset($_SESSION['accountId']) && isset($_SESSION['email']) && isset($_SESSIO
     $stmtLatestLogs->execute();
     $resultLatestLogs = $stmtLatestLogs->get_result();
 
-    $unseenCountQuery = "SELECT COUNT(*) as unseenCount FROM activitylogs WHERE seen = '3'";
+    $unseenCountQuery = "SELECT COUNT(*) as unseenCount FROM activitylogs WHERE p_seen = '3'";
     $result = $conn->query($unseenCountQuery);
     $unseenCountRow = $result->fetch_assoc();
     $unseenCount = $unseenCountRow['unseenCount'];
