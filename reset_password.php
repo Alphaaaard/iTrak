f24b61edc173672184746f82d65706226ff1b31a
Heisei <92716223+Bagacay@users.noreply.github.com>
2024-03-09 21:55:24 +0800

    UI

--- reset_password.php
+++ reset_password.php
@@ -110,6 +110,8 @@ if (isset($_GET['token'])) {
     <html lang="en">
 
     <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="./src/css/reset-password.css">
       <link rel="stylesheet" href="./src/css/main.css">
     </head>
