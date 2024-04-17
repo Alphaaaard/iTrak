0e42d29382f877c32c2674f1b2fbb4676d40ecd3
Heisei <92716223+Bagacay@users.noreply.github.com>
2024-04-17 16:08:08 +0800

    Update fetch_personnel_location.php

--- users/administrator/fetch_personnel_location.php
+++ users/administrator/fetch_personnel_location.php
@@ -67,7 +67,7 @@ $buildings = [
 
     'ballroom Building' => [
         ['lat' => 14.70055, 'long' => 121.03382, 'radius' =>  9.92],
-        ['lat' => 14.74133, 'long' => 121.06732, 'radius' =>  9.92],
+        ['lat' => 14.74151, 'long' => 121.06723, 'radius' =>  100],
 
     ],
 
