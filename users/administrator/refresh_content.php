503cf039b5af73caa04896d45fdfdf7bbf06e70e
shiki2x <161136705+shikigami2x@users.noreply.github.com>
2024-04-22 13:09:35 +0800

    Update refresh_content.php

--- users/administrator/refresh_content.php
+++ users/administrator/refresh_content.php
@@ -51,7 +51,7 @@ if ($result->num_rows > 0) {
         echo "<div id='" . $collapseId . "' class='accordion-collapse collapse' aria-labelledby='" . $headerId . "' data-bs-parent='#accordionGPS'>"; // Ensure this points to the main container ID
         echo "<div class='accordion-body'>";
         echo "Status: " . $status . "<br>";
-        echo "Timestamp: " . $timestamp . "<br>";
+    
 
         // Only display location if status is 'Online'
         if ($status === 'Online') {
