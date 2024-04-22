b6b3fc899e5b0ae3eda5b5c6fff1501a1bae06c1
Heisei <92716223+Bagacay@users.noreply.github.com>
2024-04-23 03:40:19 +0800

    Update auto_logout.php

--- auto_logout.php
+++ auto_logout.php
@@ -1,5 +1,5 @@
 <?php
-  require_once("config/connection.php");
+require_once("config/connection.php");
 session_start();
 $conn = connection();
 
@@ -8,30 +8,29 @@ date_default_timezone_set('Asia/Manila');
 
 // Check if the user is logged in
 if (isset($_SESSION['accountId'])) {
-    // Get the current time
-    $currentTime = date("H:i:s");
+  // Get the current time
+  $currentTime = date("H:i:s");
 
-    // Check if it's midnight (00:00:00)
-    if ($currentTime === "00:00:00") {
-        // Clear the latitude and longitude data in the 'account' table for the user
-      
-     
+  // Check if it's midnight (00:00:00)
+  if ($currentTime === "00:00:00") {
+    // Clear the latitude and longitude data in the 'account' table for the user
 
-      
-        $sql = "UPDATE account SET latitude = NULL, longitude = NULL, timestamp = NULL, qculocation = NULL WHERE accountId = ?";
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param("i", $accountId);
-        $stmt->execute();
-        $stmt->close();
 
-        // Destroy the session
-        unset($_SESSION['accountId']);
-        session_destroy();
 
-        // Redirect the user to the "index.php" page
-        header("Location: ../../index.php");
-        exit();
-    }
+    $sql = "UPDATE account SET latitude = NULL, longitude = NULL, timestamp = NULL WHERE accountId = ?";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $accountId);
+    $stmt->execute();
+    $stmt->close();
+
+    // Destroy the session
+    unset($_SESSION['accountId']);
+    session_destroy();
+
+    // Redirect the user to the "index.php" page
+    header("Location: ../../index.php");
+    exit();
+  }
 }
-?>
