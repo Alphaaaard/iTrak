b4e19e576b94677f521c88c366523e4de6285485
Yohanskie101 <125958519+Yohanskie101@users.noreply.github.com>
2024-04-28 05:18:50 +0800

    Sorry

---
+++ src/js/batasan.js
@@ -0,0 +1,128 @@
+function showAddConfirmation() {
+  Swal.fire({
+      icon: "info",
+      title: `Are you sure you want to add this task?`,
+      showCancelButton: true,
+      cancelButtonText: "No",
+      focusConfirm: false,
+      confirmButtonText: "Yes",
+  })
+  .then((result) => {
+      if (result.isConfirmed) {
+        let swalConfirm = document.querySelector(".swal2-confirm");
+        swalConfirm.setAttribute("name", "add");
+  
+        // AJAX
+        let form = document.querySelector("#addrequestForm");
+        let xhr = new XMLHttpRequest();
+  
+        xhr.open("POST", "../../users/administrator/batasan.php", true);
+  
+        xhr.onerror = function () {
+          console.error("An error occurred during the XMLHttpRequest");
+        };
+  
+        let formData = new FormData(form);
+        formData.set("add", swalConfirm);
+        xhr.send(formData);
+  
+        // success alertbox
+        Swal.fire({
+          text: "The task has been successfully added",
+          icon: "success",
+          timer: 1000,
+          showConfirmButton: false,
+        }).then((result) => {
+          if (result.dismiss || Swal.DismissReason.timer) {
+            window.location.reload();
+          }
+        });
+      }
+  });
+}
+
+function showOutsourceConfirmation() {
+  Swal.fire({
+      icon: "info",
+      title: `Are you sure you want to outsource this task?`,
+      showCancelButton: true,
+      cancelButtonText: "No",
+      focusConfirm: false,
+      confirmButtonText: "Yes",
+  })
+  .then((result) => {
+      if (result.isConfirmed) {
+        let swalConfirm = document.querySelector(".swal2-confirm");
+        swalConfirm.setAttribute("name", "done");
+  
+        // AJAX
+        let form = document.querySelector("#outsourceForm");
+        let xhr = new XMLHttpRequest();
+  
+        xhr.open("POST", "../../users/administrator/batasan.php", true);
+  
+        xhr.onerror = function () {
+          console.error("An error occurred during the XMLHttpRequest");
+        };
+  
+        let formData = new FormData(form);
+        formData.set("outsource", swalConfirm);
+        xhr.send(formData);
+  
+        // success alertbox
+        Swal.fire({
+          text: "The task has been outsourced!",
+          icon: "success",
+          timer: 1000,
+          showConfirmButton: false,
+        }).then((result) => {
+          if (result.dismiss || Swal.DismissReason.timer) {
+            window.location.reload();
+          }
+        });
+      }
+  });
+}
+
+function showApprovalConfirmation() {
+  Swal.fire({
+      icon: "info",
+      title: `Are you sure you want to approve this task?`,
+      showCancelButton: true,
+      cancelButtonText: "No",
+      focusConfirm: false,
+      confirmButtonText: "Yes",
+  })
+  .then((result) => {
+      if (result.isConfirmed) {
+        let swalConfirm = document.querySelector(".swal2-confirm");
+        swalConfirm.setAttribute("name", "done");
+  
+        // AJAX
+        let form = document.querySelector("#approvalForm");
+        let xhr = new XMLHttpRequest();
+  
+        xhr.open("POST", "../../users/administrator/batasan.php", true);
+  
+        xhr.onerror = function () {
+          console.error("An error occurred during the XMLHttpRequest");
+        };
+  
+        let formData = new FormData(form);
+        formData.set("approval", swalConfirm);
+        xhr.send(formData);
+  
+        // success alertbox
+        Swal.fire({
+          text: "The task has been successfully approved",
+          icon: "success",
+          timer: 1000,
+          showConfirmButton: false,
+        }).then((result) => {
+          if (result.dismiss || Swal.DismissReason.timer) {
+            window.location.reload();
+          }
+        });
+      }
+  });
+}
