b3e8c2c4da6627ae3326ebd91ee8990f2dd6e58a
Heisei <92716223+Bagacay@users.noreply.github.com>
2024-04-30 03:32:23 +0800

    Con

--- config/connection1.php
+++ config/connection1.php
@@ -1,9 +1,9 @@
 <?php
 // LOCAL
-$conn = mysqli_connect("localhost", "root", "", "upkeep");
+// $conn = mysqli_connect("localhost", "root", "", "upkeep");
 
 //HOSTED
-// $conn = mysqli_connect("localhost", "u579600805_iTrak", "iTrak123", "u579600805_iTrak");
+$conn = mysqli_connect("localhost", "u579600805_iTrak", "iTrak123", "u579600805_iTrak");
 
 if (!$conn) {
     die('Connection failed!' . mysqli_connect_error());
