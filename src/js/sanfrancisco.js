0a415e17d255e0193996a10e59c182a00043c1c5
TatsuyaxShiba <daniel.quinto015@gmail.com>
2024-05-02 05:01:37 +0800

    ggs

--- src/js/sanfrancisco.js
+++ src/js/sanfrancisco.js
@@ -1,129 +1,125 @@
 function showAddConfirmation() {
-    Swal.fire({
-        icon: "info",
-        title: `Are you sure you want to add this task?`,
-        showCancelButton: true,
-        cancelButtonText: "No",
-        focusConfirm: false,
-        confirmButtonText: "Yes",
-    })
-    .then((result) => {
-        if (result.isConfirmed) {
-          let swalConfirm = document.querySelector(".swal2-confirm");
-          swalConfirm.setAttribute("name", "add");
-    
-          // AJAX
-          let form = document.querySelector("#addrequestForm");
-          let xhr = new XMLHttpRequest();
-    
-          xhr.open("POST", "../../users/administrator/sanFrancisco.php", true);
-    
-          xhr.onerror = function () {
-            console.error("An error occurred during the XMLHttpRequest");
-          };
-    
-          let formData = new FormData(form);
-          formData.set("add", swalConfirm);
-          xhr.send(formData);
-    
-          // success alertbox
-          Swal.fire({
-            text: "The task has been successfully added",
-            icon: "success",
-            timer: 1000,
-            showConfirmButton: false,
-          }).then((result) => {
-            if (result.dismiss || Swal.DismissReason.timer) {
-              window.location.reload();
-            }
-          });
+  Swal.fire({
+    icon: "info",
+    title: `Are you sure you want to add this task?`,
+    showCancelButton: true,
+    cancelButtonText: "No",
+    focusConfirm: false,
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let swalConfirm = document.querySelector(".swal2-confirm");
+      swalConfirm.setAttribute("name", "add");
+
+      // AJAX
+      let form = document.querySelector("#addrequestForm");
+      let xhr = new XMLHttpRequest();
+
+      xhr.open("POST", "../../users/administrator/sanFrancisco.php", true);
+
+      xhr.onerror = function () {
+        console.error("An error occurred during the XMLHttpRequest");
+      };
+
+      let formData = new FormData(form);
+      formData.set("add", swalConfirm);
+      xhr.send(formData);
+
+      // success alertbox
+      Swal.fire({
+        text: "The task has been successfully added",
+        icon: "success",
+        timer: 1000,
+        showConfirmButton: false,
+      }).then((result) => {
+        if (result.dismiss || Swal.DismissReason.timer) {
+          window.location.reload();
         }
-    });
-  }
-  
-  function showOutsourcesConfirmation() {
-    Swal.fire({
-        icon: "info",
-        title: `Are you sure you want to mark this task as completed?`,
-        showCancelButton: true,
-        cancelButtonText: "No",
-        focusConfirm: false,
-        confirmButtonText: "Yes",
-    })
-    .then((result) => {
-        if (result.isConfirmed) {
-          let swalConfirm = document.querySelector(".swal2-confirm");
-          swalConfirm.setAttribute("name", "done");
-    
-          // AJAX
-          let form = document.querySelector("#outsourcesForm");
-          let xhr = new XMLHttpRequest();
-    
-          xhr.open("POST", "../../users/administrator/sanFrancisco.php", true);
-    
-          xhr.onerror = function () {
-            console.error("An error occurred during the XMLHttpRequest");
-          };
-    
-          let formData = new FormData(form);
-          formData.set("outsource", swalConfirm);
-          xhr.send(formData);
-    
-          // success alertbox
-          Swal.fire({
-            text: "The task has been marked as done!",
-            icon: "success",
-            timer: 1000,
-            showConfirmButton: false,
-          }).then((result) => {
-            if (result.dismiss || Swal.DismissReason.timer) {
-              window.location.reload();
-            }
-          });
+      });
+    }
+  });
+}
+
+function showOutsourcesConfirmation() {
+  Swal.fire({
+    icon: "info",
+    title: `Are you sure you want to mark this task as completed?`,
+    showCancelButton: true,
+    cancelButtonText: "No",
+    focusConfirm: false,
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let swalConfirm = document.querySelector(".swal2-confirm");
+      swalConfirm.setAttribute("name", "outsource");
+
+      // AJAX
+      let form = document.querySelector("#outsourcesForm");
+      let xhr = new XMLHttpRequest();
+
+      xhr.open("POST", "../../users/administrator/sanFrancisco.php", true);
+
+      xhr.onerror = function () {
+        console.error("An error occurred during the XMLHttpRequest");
+      };
+
+      let formData = new FormData(form);
+      formData.set("outsource", swalConfirm);
+      xhr.send(formData);
+
+      // success alertbox
+      Swal.fire({
+        text: "The task has been marked as done!",
+        icon: "success",
+        timer: 1000,
+        showConfirmButton: false,
+      }).then((result) => {
+        if (result.dismiss || Swal.DismissReason.timer) {
+          window.location.reload();
         }
-    });
-  }
-  
-  function showApprovalConfirmation() {
-    Swal.fire({
-        icon: "info",
-        title: `Are you sure you want to approve this task?`,
-        showCancelButton: true,
-        cancelButtonText: "No",
-        focusConfirm: false,
-        confirmButtonText: "Yes",
-    })
-    .then((result) => {
-        if (result.isConfirmed) {
-          let swalConfirm = document.querySelector(".swal2-confirm");
-          swalConfirm.setAttribute("name", "done");
-    
-          // AJAX
-          let form = document.querySelector("#approvalForm");
-          let xhr = new XMLHttpRequest();
-    
-          xhr.open("POST", "../../users/administrator/sanFrancisco.php", true);
-    
-          xhr.onerror = function () {
-            console.error("An error occurred during the XMLHttpRequest");
-          };
-    
-          let formData = new FormData(form);
-          formData.set("approval", swalConfirm);
-          xhr.send(formData);
-    
-          // success alertbox
-          Swal.fire({
-            text: "The task has been successfully approved",
-            icon: "success",
-            timer: 1000,
-            showConfirmButton: false,
-          }).then((result) => {
-            if (result.dismiss || Swal.DismissReason.timer) {
-              window.location.reload();
-            }
-          });
+      });
+    }
+  });
+}
+
+function showApprovalConfirmation() {
+  Swal.fire({
+    icon: "info",
+    title: `Are you sure you want to approve this task?`,
+    showCancelButton: true,
+    cancelButtonText: "No",
+    focusConfirm: false,
+    confirmButtonText: "Yes",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let swalConfirm = document.querySelector(".swal2-confirm");
+      swalConfirm.setAttribute("name", "approval");
+
+      // AJAX
+      let form = document.querySelector("#approvalForm");
+      let xhr = new XMLHttpRequest();
+
+      xhr.open("POST", "../../users/administrator/sanFrancisco.php", true);
+
+      xhr.onerror = function () {
+        console.error("An error occurred during the XMLHttpRequest");
+      };
+
+      let formData = new FormData(form);
+      formData.set("approval", swalConfirm);
+      xhr.send(formData);
+
+      // success alertbox
+      Swal.fire({
+        text: "The task has been successfully approved",
+        icon: "success",
+        timer: 1000,
+        showConfirmButton: false,
+      }).then((result) => {
+        if (result.dismiss || Swal.DismissReason.timer) {
+          window.location.reload();
         }
-    });
-  }
-  
+      });
+    }
+  });
+}
