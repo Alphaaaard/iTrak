e07bb62ed70a9192ca12b2f6ec4dd5194ff8124a
shiki2x <161136705+shikigami2x@users.noreply.github.com>
2024-05-02 00:41:27 +0800

    Update request.php

--- users/personnel/request.php
+++ users/personnel/request.php
@@ -225,7 +225,7 @@ WHERE p_seen = '0' AND accountID != ? AND action LIKE 'Assigned maintenance pers
         $deadline2 = $_POST['deadline'];
 
         // Calculate the current date plus 8 hours
-        $adjusted_date = date('Y-m-d H:i:s', strtotime('+0 hours'));
+        $adjusted_date = date('Y-m-d H:i:s', strtotime('+8 hours'));
 
         // Retrieve selected return_reason from radio buttons
         $return_reason = $_POST['return_reason'];
