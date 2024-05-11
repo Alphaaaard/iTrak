86d817ac40eb51bba22c8ef6ff7a4316067ffd0d
Heisei <92716223+Bagacay@users.noreply.github.com>
2024-05-11 20:38:00 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -2,16 +2,16 @@
 function connection()
 {
     // LOCAL
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "upkeep";
+    // $host = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "upkeep";
 
     // HOSTED
-    // $host = "localhost";
-    // $username = "u579600805_iTrak";
-    // $password = "iTrak123";
-    // $database = "u579600805_iTrak";
+    $host = "localhost";
+    $username = "u579600805_iTrak";
+    $password = "iTrak123";
+    $database = "u579600805_iTrak";
 
     $conn = new mysqli($host, $username, $password, $database);
 
