ac42c6cecd4ec8787a04181684392585d554b540
TatsuyaxShiba <daniel.quinto015@gmail.com>
2024-05-11 19:05:16 +0800

    Task Completion

--- config/connection.php
+++ config/connection.php
@@ -1,17 +1,17 @@
 <?php
 function connection()
 {
-    //LOCAL
-    // $host = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $database = "upkeep";
+    // LOCAL
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "upkeep";
 
     // HOSTED
-    $host = "localhost";
-    $username = "u579600805_iTrak";
-    $password = "iTrak123";
-    $database = "u579600805_iTrak";
+    // $host = "localhost";
+    // $username = "u579600805_iTrak";
+    // $password = "iTrak123";
+    // $database = "u579600805_iTrak";
 
     $conn = new mysqli($host, $username, $password, $database);
 
